370907678144673e93c58d19cc62d73a3dd8d408
erangeljr <hacker@eddierangel.com>
2014-01-28 21:23:32 -0800

    Pushing Jekyll Site

---
+++ js/site.js
@@ -0,0 +1,29 @@
+(function(){
+	"use strict";
+
+	var $pickButton = $("#pickButton");
+
+	$("#reasonDropdown li a").on("click", function(){
+		var reason = $(this).text();
+		$pickButton.text(reason);
+	});
+
+
+	var $sentDialog =$("#sentDialog");
+
+	$("#contactForm").on("submit", function(){
+		$sentDialog.modal('show');
+		return false;
+	});
+
+	$sentDialog.on("hidden.bs.modal", function(){
+		alert("close");
+	});
+
+	$("#contactForm input[type=submit]").tooltip({
+		delay: {
+			show: 500,
+			hide: 0
+		}
+	});
+})();
